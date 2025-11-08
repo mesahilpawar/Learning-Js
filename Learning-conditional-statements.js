063fe34609b08c90c8f0277d6867d7b0420ebda6
Sahil1222hyper <pawarsahi2002@gmail.com>
2025-11-08 14:23:51 +0530

    making it as functional statement

--- Learning-conditional-statements.js
+++ Learning-conditional-statements.js
@@ -1,23 +1,25 @@
 const readline = require ('readline-sync');
 
-// taking user input
-let userInput = readline.question('Enter your age ');
-
-// parsing the number in integer format
-let age = Number(userInput);
-
 // License age checker
-switch (true) {
-    case (age >= 18):
-        console.log('Your age is valid for driving');
-        break
-    case (age < 18):
-        let gap = 18 - age;
-        console.log('your age is not valid for the driving after ', gap, ' years later you are eligible for the driving')
-        break
-    case (age == 0):
-        console.log('You can not add zero as age')
-        break
-    default:
-        console.log('Please enter valid number')
+function licenseChecker(){
+    // taking user input
+    let userInput = readline.question('Enter your age ');
+    
+    // parsing the number in integer format
+    let age = Number(userInput);
+
+    switch (true) {
+        case (age >= 18):
+            console.log('Your age is valid for driving');
+            break
+        case (age < 18):
+            let gap = 18 - age;
+            console.log('your age is not valid for the driving after ', gap, ' years later you are eligible for the driving')
+            break
+        case (age == 0):
+            console.log('You can not add zero as age')
+            break
+        default:
+            console.log('Please enter valid number')
+    }
 }
