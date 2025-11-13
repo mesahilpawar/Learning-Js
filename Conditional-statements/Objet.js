c28f683c0c234ab657b01e8556bfc60c6b1f0478
Sahil1222hyper <pawarsahi2002@gmail.com>
2025-11-13 09:51:46 +0530

    Learning Object method

--- Conditional-statements/Objet.js
+++ Conditional-statements/Objet.js
@@ -1,10 +1,27 @@
-const person ={
-    FirstName : 'Sahil',
+// const person ={
+//     FirstName : 'Sahil',
+//     LastName : 'pawar',
+//     Age : 22,
+//     Country : 'India',
+//     Skills : ['Html', 'Css', 'Python', 'Javascript', 'React', 'C++', 'Nodejs']
+// };
+// person.Title = 'Teacher'
+// person.Skills.push = ('MongoDB')
+// console.log(person);
+
+// Object Method
+const person = {
+    FirstName : 'sahil',
     LastName : 'pawar',
     Age : 22,
     Country : 'India',
-    Skills : ['Html', 'Css', 'Python', 'Javascript', 'React', 'C++', 'Nodejs']
-};
-person.Title = 'Teacher'
-person.Skills.push = ('MongoDB')
-console.log(person);
+    Skills : ['Html', 'Css', 'Javascript', 'Nodejs', 'React'],
+    Title : 'Teacher',
+    Address : {
+        Street : 'HV colony mg road',
+        Pin : 263546,
+        City : 'Chh. sambhajinagar',
+    }
+}
+    const copyPerson = Object.assign({}, person) 
+    console.log(copyPerson)
