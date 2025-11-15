8580e7381e6be13084fa74cbb0d9b15088bcb573
Sahil1222hyper <pawarsahi2002@gmail.com>
2025-11-15 11:32:59 +0530

    Learning the function with 1/2 arguments

--- Conditional-statements/Function.js
+++ Conditional-statements/Function.js
@@ -1,9 +1,32 @@
-function fullName () {
-    let FirstName = 'sahil'
-    let LastName = 'pawar'
-    let space = " "
-    let fullName = FirstName+ space +LastName;
-    console.log(fullName)
+// function printFullName () {
+//     let FirstName = 'sahil'
+//     let LastName = 'pawar'
+//     let space = " "
+//     let fullName = FirstName+ space +LastName
+//     return fullName
 
+// }
+// console.log(printFullName());
+
+// Adding the 2 number in function 
+// function addTwoNumber() {
+//     let num1 = 12
+//     let num2 = 4
+//     let total = num1 + num2
+//     return total
+// }
+// console.log(addTwoNumber())
+
+// Function with 1 arguments 
+// function areaOfCircle(r) {
+//     let area = Math.PI*r*r
+//     return area
+// }
+// console.log(areaOfCircle(10))
+
+// Function with 2 arguments 
+function sumTwoNum (num1, num2) {
+    let sum = num1 + num2
+    return sum 
 }
-fullName()
+console.log(sumTwoNum(10, 20))
