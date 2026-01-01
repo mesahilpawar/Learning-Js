f7afd4fb727c2ea4a255fddaffea8e21ee160902
Sahil1222hyper <pawarsahi2002@gmail.com>
2026-01-01 11:49:56 +0530

    Doing Function exercise

---
+++ Conditional-statements/Function-Exercise1.js
@@ -0,0 +1,16 @@
+// exercise of functions 
+// function fullName (FirstName ='sahil', lastName ='pawar'){
+//     let space = ' '
+//     let fullName = FirstName + space + lastName
+//     return fullName
+// }
+// console.log(fullName())
+// console.log(fullName ('aniket', 'sharma'))
+
+//exercise no. 2
+function addNumbers (a = 5 , b = 8){
+    let sum = a + b 
+    return sum
+}
+console.log(addNumbers())
+console.log(addNumbers(10,15))
