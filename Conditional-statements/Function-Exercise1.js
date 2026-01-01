50bffccbae19a9d10759e43274951d19aca59e61
Sahil1222hyper <pawarsahi2002@gmail.com>
2026-01-01 12:29:08 +0530

    Checking the season by user provided months

--- Conditional-statements/Function-Exercise1.js
+++ Conditional-statements/Function-Exercise1.js
@@ -8,9 +8,37 @@
 // console.log(fullName ('aniket', 'sharma'))
 
 //exercise no. 2
-function addNumbers (a = 5 , b = 8){
-    let sum = a + b 
-    return sum
+// function addNumbers (a = 5 , b = 8){
+//     let sum = a + b 
+//     return sum
+// }
+// console.log(addNumbers())
+// console.log(addNumbers(10,15))
+
+//exercise no.3
+const readline = require ('readline-sync');
+
+function checkSeason (){
+    // takes input from the user
+    let userInput = readline.question('Enter the month :');
+    let Season = userInput.toLowerCase(); // convert them in lower case
+
+    // checking the season with user's entered season 
+    if (Season === 'march' || Season === 'april' || Season === 'may'){
+        console.log("This season is Spring")
+        return "Spring";
+    } else if (Season === 'june' || Season === 'july' || Season === 'august'){
+        console.log("This season is Summer");
+        return "Summer";
+    }else if (Season === 'september' || Season === 'october' || Season === 'november'){
+        console.log("This season is Autumn");
+        return "Autumn";
+    }else if (Season === 'december' || Season === 'january' || Season === 'february'){
+        console.log("This season is Winter");
+        return "Winter";
+    }
+
 }
-console.log(addNumbers())
-console.log(addNumbers(10,15))
+
+const season = checkSeason();
+console.log(season);
