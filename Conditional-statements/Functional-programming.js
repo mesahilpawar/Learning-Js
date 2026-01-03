02f82a733b9afeb0d84d9d3f69991a65e05a2584
Sahil1222hyper <pawarsahi2002@gmail.com>
2026-01-03 13:29:45 +0530

    Example of map method for simple function

--- Conditional-statements/Functional-programming.js
+++ Conditional-statements/Functional-programming.js
@@ -1,5 +1,12 @@
-const Countries = ['India', 'Iceland', 'America', 'French']
-const newCountries = Countries.map(function (country){
-    return country.toLocaleUpperCase()
+// const Countries = ['India', 'Iceland', 'America', 'French']
+// const newCountries = Countries.map(function (country){
+//     return country.toLocaleUpperCase()
+// })
+// console.log(newCountries)
+
+// Example of map methods 
+const Names = ['sai', 'ram', 'sham', 'ben', 'lin']
+const newName = Names.map (function (Name){
+    return Name.toLocaleUpperCase()
 })
-console.log(newCountries)
+console.log(newName)
