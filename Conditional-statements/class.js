4801ae953d2a44f8041c460647b45c0ede01668e
Sahil1222hyper <pawarsahi2002@gmail.com>
2026-01-04 15:59:31 +0530

    Doing exercise of class

--- Conditional-statements/class.js
+++ Conditional-statements/class.js
@@ -1,14 +1,33 @@
-class person {
-    constructor (FirstName, LastName, Age, Title){
-        this.FirstName = FirstName;
-        this.LastName = LastName;
-        this.Age = Age;
-        this.Title = Title;
+// class person {
+//     constructor (FirstName, LastName, Age, Title){
+//         this.FirstName = FirstName;
+//         this.LastName = LastName;
+//         this.Age = Age;
+//         this.Title = Title;
+//     }
+//     getFullInfo(){
+//         const fullInfo = this.FirstName + "" + this.LastName + "" + this.Age + "" + this.Title
+//         return fullInfo
+//     }
+// }
+// const person1 = new person ('sahil', 'pawar', 23, 'Soft Engg.');
+// const person2 = new person ('sai', 'patil', 25, 'Electric Engg.');
+// console.log(person1);
+// console.log(person2);
+
+
+
+class personInfo {
+    constructor (name, marks, ranks, grank ){
+        this.name = name ;
+        this.marks = marks ;
+        this.ranks = ranks ;
+        this.grank = grank;
     }
-    getFullInfo(){
-        const fullInfo = this.FirstName + "" + this.LastName + "" + this.Age + "" + this.Title
-        return fullInfo
+    getFullInfo (){
+        const fullInfo = this.name + "" + this.marks + "" + this.ranks + "" + this.grank
+        return fullInfo;
     }
 }
-const person1 = new person ('sahil', 'pawar', "23", 'Soft Engg.')
-console.log(person1)
+const Fullinfo = new personInfo('sahil', 340, 15000, 70000);
+console.log(Fullinfo);
